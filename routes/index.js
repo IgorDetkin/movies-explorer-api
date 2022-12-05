b2b21811f07376d62bf5c38eccbb063a865ab693
Igor Detkin <dean94@bk.ru>
2022-12-06 03:44:33 +0600

    backend_correct_regex

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,19 @@ const { login, createUser } = require('../controllers/users');
 const auth = require('../middlewares/auth');
 const NotFoundError = require('../middlewares/errors/NotFoundError');
 
+
+const regexName = /[a-zA-Z0-9А-Яа-яЁё._:$!%@\s-]+/;
+const regexEmail = /^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+\.+(\w{2,})$/;
+const regexPassword = /[a-zA-Z0-9._\W\s-]+/;
+
+
 router.post(
   '/signup',
   celebrate({
     body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(30),
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
+      name: Joi.string().required().min(2).max(30).pattern(regexName),
+      email: Joi.string().required().email().pattern(regexEmail),
+      password: Joi.string().required().pattern(regexPassword),
     }),
   }),
   createUser,
@@ -20,8 +26,8 @@ router.post(
   '/signin',
   celebrate({
     body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
+      email: Joi.string().required().email().pattern(regexEmail),
+      password: Joi.string().required().pattern(regexPassword),
     }),
   }),
   login,
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const { getUserInfo, updateProfile } = require('../controllers/users');
+const regexName = /[a-zA-Z0-9А-Яа-яЁё._:$!%@\s-]+/;
+const regexEmail = /^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+\.+(\w{2,})$/;
 
 router.get('/me', getUserInfo);
 
@@ -8,8 +10,8 @@ router.patch(
   '/me',
   celebrate({
     body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(30),
-      email: Joi.string().required().email(),
+      name: Joi.string().required().min(2).max(30).pattern(regexName),
+      email: Joi.string().required().email().pattern(regexEmail),
     }),
   }),
   updateProfile,
